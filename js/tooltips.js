2840847503da777569d8653e2dd627f5f90cfb49
darkoto12 <darkoto12@gmail.com>
2025-01-04 00:36:46 +0200

    add profile, update icons

--- js/tooltips.js
+++ js/tooltips.js
@@ -41,13 +41,32 @@ const statusButtons=document.querySelectorAll('.list__table-status');tippy(statu
          <button class="tippy-menu-button error">
           <i class="ui-icon-error"></i>
           Zdjęcie profilowe
-         </button>         
+         </button>
        </div>
        `,allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});const addButtons=document.querySelectorAll('.list__add-button');tippy(addButtons,{content:e=>{const menu=document.createElement('div');menu.classList.add('tippy-menu');menu.style.width=`${e.clientWidth}px`;menu.innerHTML=`
         <button class="tippy-menu-button" data-popup-open="popup-invite-driver">
-          Wysłać zaproszenie 
-          </button>  
+          Wysłać zaproszenie
+          </button>
           <button class="tippy-menu-button" data-popup-open="popup-add-driver">
-          Dodaj do systemy 
-        </button>      
-    `;const buttons=menu.querySelectorAll('button');buttons.forEach(btn=>{btn.onclick=()=>{e._tippy.hide();};});return menu;},allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});
+          Dodaj do systemy
+        </button>
+    `;const buttons=menu.querySelectorAll('button');buttons.forEach(btn=>{btn.onclick=()=>{e._tippy.hide();};});return menu;},allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});const downloadButtons=document.querySelectorAll('.list__table-download-button');tippy(downloadButtons,{content:`
+       <div class="tippy-menu">
+         <button class="tippy-menu-button">
+          <i class="ui-icon-download"></i>
+          Nazwa i nr fv
+         </button>
+         <button class="tippy-menu-button">
+          <i class="ui-icon-download"></i>
+          Nazwa i nr fv
+         </button>
+         <button class="tippy-menu-button">
+          <i class="ui-icon-download"></i>
+          Nazwa i nr fv
+         </button>
+         <button class="tippy-menu-button">
+          <i class="ui-icon-download"></i>
+          Nazwa i nr fv
+         </button>
+       </div>
+       `,allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});const profileButtons=document.querySelectorAll('.header__profile');tippy(profileButtons,{content:e=>{const menu=document.createElement('div');menu.classList.add('tippy-menu');renderButtons(menu,[{text:'Mój profil',handleClick:()=>{const profile=document.querySelector('.profile');profile.classList.add('active');},},{text:'Ustawienia',},{text:'Dodaj nowe miasto',},{text:'Wyloguj się',},],e,);return menu;},allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});function renderButtons(container,buttons,parent){buttons.forEach(btn=>{const button=document.createElement('button');button.classList.add('tippy-menu-button');button.textContent=btn.text;button.onclick=()=>{btn.handleClick();parent._tippy.hide();};container.appendChild(button);});}
