9a4f2bcd6670a904b4f321d427569766116f3d88
darkoto12 <darkoto12@gmail.com>
2025-01-05 10:48:39 +0200

    fix

--- js/tooltips.js
+++ js/tooltips.js
@@ -1,5 +1,7 @@
+const statusButtons = document.querySelectorAll('.list__table-status');
 
-const statusButtons=document.querySelectorAll('.list__table-status');tippy(statusButtons,{content:`
+tippy(statusButtons, {
+  content: `
        <div class="tippy-status">
          <span class="tippy-title">
           Brak dokumentów
@@ -8,7 +10,18 @@ const statusButtons=document.querySelectorAll('.list__table-status');tippy(statu
           Brakują badanie lekarskie, psychotechnika, zdjęcie profilowe
          </p>
        </div>
-       `,allowHTML:true,placement:'bottom-start',arrow:false,interactive:true,trigger:'click',});const dotButtons=document.querySelectorAll('.list__table-button-dots');tippy(dotButtons,{content:`
+       `,
+  allowHTML: true,
+  placement: 'bottom-start',
+  arrow: false,
+  interactive: true,
+  trigger: 'click',
+});
+
+const dotButtons = document.querySelectorAll('.list__table-button-dots');
+
+tippy(dotButtons, {
+  content: `
        <div class="tippy-menu">
          <button class="tippy-menu-button">
           <i class="ui-icon-error"></i>
@@ -19,7 +32,18 @@ const statusButtons=document.querySelectorAll('.list__table-status');tippy(statu
           Zablokuj gotówkę
          </button>
        </div>
-       `,allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});const docStatusButtons=document.querySelectorAll('.list__doc-verf');tippy(docStatusButtons,{content:`
+       `,
+  allowHTML: true,
+  placement: 'bottom-end',
+  arrow: false,
+  interactive: true,
+  trigger: 'click',
+});
+
+const docStatusButtons = document.querySelectorAll('.list__doc-verf');
+
+tippy(docStatusButtons, {
+  content: `
        <div class="tippy-menu">
          <p class="tippy-menu-title">Dowód osobisty</p>
          <button class="tippy-menu-button success">
@@ -43,14 +67,50 @@ const statusButtons=document.querySelectorAll('.list__table-status');tippy(statu
           Zdjęcie profilowe
          </button>
        </div>
-       `,allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});const addButtons=document.querySelectorAll('.list__add-button');tippy(addButtons,{content:e=>{const menu=document.createElement('div');menu.classList.add('tippy-menu');menu.style.width=`${e.clientWidth}px`;menu.innerHTML=`
+       `,
+  allowHTML: true,
+  placement: 'bottom-end',
+  arrow: false,
+  interactive: true,
+  trigger: 'click',
+});
+
+const addButtons = document.querySelectorAll('.list__add-button');
+tippy(addButtons, {
+  content: e => {
+    const menu = document.createElement('div');
+    menu.classList.add('tippy-menu');
+    menu.style.width = `${e.clientWidth}px`;
+
+    menu.innerHTML = `
         <button class="tippy-menu-button" data-popup-open="popup-invite-driver">
           Wysłać zaproszenie
           </button>
           <button class="tippy-menu-button" data-popup-open="popup-add-driver">
           Dodaj do systemy
         </button>
-    `;const buttons=menu.querySelectorAll('button');buttons.forEach(btn=>{btn.onclick=()=>{e._tippy.hide();};});return menu;},allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});const downloadButtons=document.querySelectorAll('.list__table-download-button');tippy(downloadButtons,{content:`
+    `;
+
+    const buttons = menu.querySelectorAll('button');
+    buttons.forEach(btn => {
+      btn.onclick = () => {
+        e._tippy.hide();
+      };
+    });
+
+    return menu;
+  },
+  allowHTML: true,
+  placement: 'bottom-end',
+  arrow: false,
+  interactive: true,
+  trigger: 'click',
+});
+
+const downloadButtons = document.querySelectorAll('.list__table-download-button');
+
+tippy(downloadButtons, {
+  content: `
        <div class="tippy-menu">
          <button class="tippy-menu-button">
           <i class="ui-icon-download"></i>
@@ -69,4 +129,63 @@ const statusButtons=document.querySelectorAll('.list__table-status');tippy(statu
           Nazwa i nr fv
          </button>
        </div>
-       `,allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});const profileButtons=document.querySelectorAll('.header__profile');tippy(profileButtons,{content:e=>{const menu=document.createElement('div');menu.classList.add('tippy-menu');renderButtons(menu,[{text:'Mój profil',handleClick:()=>{const profile=document.querySelector('.profile');profile.classList.add('active');},},{text:'Ustawienia',},{text:'Dodaj nowe miasto',},{text:'Wyloguj się',},],e,);return menu;},allowHTML:true,placement:'bottom-end',arrow:false,interactive:true,trigger:'click',});function renderButtons(container,buttons,parent){buttons.forEach(btn=>{const button=document.createElement('button');button.classList.add('tippy-menu-button');button.textContent=btn.text;button.onclick=()=>{btn.handleClick();parent._tippy.hide();};container.appendChild(button);});}
+       `,
+  allowHTML: true,
+  placement: 'bottom-end',
+  arrow: false,
+  interactive: true,
+  trigger: 'click',
+});
+
+const profileButtons = document.querySelectorAll('.header__profile');
+
+tippy(profileButtons, {
+  content: e => {
+    const menu = document.createElement('div');
+    menu.classList.add('tippy-menu');
+    // menu.style.width = `${e.clientWidth}px`;
+
+    renderButtons(
+      menu,
+      [
+        {
+          text: 'Mój profil',
+          handleClick: () => {
+            const profile = document.querySelector('.profile');
+            profile.classList.add('active');
+          },
+        },
+        {
+          text: 'Ustawienia',
+        },
+        {
+          text: 'Dodaj nowe miasto',
+        },
+        {
+          text: 'Wyloguj się',
+        },
+      ],
+      e,
+    );
+
+    return menu;
+  },
+  allowHTML: true,
+  placement: 'bottom-end',
+  arrow: false,
+  interactive: true,
+  trigger: 'click',
+});
+
+function renderButtons(container, buttons, parent) {
+  buttons.forEach(btn => {
+    const button = document.createElement('button');
+    button.classList.add('tippy-menu-button');
+    button.textContent = btn.text;
+    button.onclick = () => {
+      btn.handleClick();
+      parent._tippy.hide();
+    };
+    container.appendChild(button);
+  });
+}
--- js/zarobki.js
+++ js/zarobki.js
@@ -1,3 +1,35 @@
+const info = document.querySelector('.info');
+const sidebarButtons = document.querySelectorAll('.list__table-item-top');
+const moreButtons = document.querySelectorAll('.list__table-item-more');
+const sidebarUser = document.querySelector('.info__body.user');
+const table = document.querySelector('.info__body');
+const total = document.querySelector('.filtr');
+sidebarButtons.forEach(button => {
+  button.onclick = () => {
+    if (window.innerWidth <= 680) {
+      return;
+    }
 
-const info=document.querySelector('.info');const arrowButtons=document.querySelectorAll('.list__table-button-arrow');const moreButtons=document.querySelectorAll('.list__table-item-more');const sidebarUser=document.querySelector('.info__body.user');const table=document.querySelector('.info__body');const total=document.querySelector('.filtr');arrowButtons.forEach(button=>{button.onclick=()=>{if(window.innerWidth<=680){return;}
-total.style.display='none';sidebarUser.style.display='flex';info.classList.add('right-0');};});moreButtons.forEach(button=>{button.onclick=()=>{total.style.display='none';table.style.display='none';sidebarUser.style.display='flex';info.classList.add('right-0');};});const closeUserButton=document.querySelector('.user__close');closeUserButton.onclick=()=>{total.style.display='flex';table.style.display='flex';sidebarUser.style.display='none';info.classList.remove('right-0');};
+    total.style.display = 'none';
+    sidebarUser.classList.add('active');
+    info.classList.add('right-0');
+  };
+});
+
+moreButtons.forEach(button => {
+  button.onclick = () => {
+    total.style.display = 'none';
+    table.style.display = 'none';
+    sidebarUser.classList.add('active');
+    info.classList.add('right-0');
+  };
+});
+
+const closeUserButton = document.querySelector('.user__close');
+
+closeUserButton.onclick = () => {
+  total.style.display = 'flex';
+  table.style.display = 'flex';
+  sidebarUser.classList.remove('active');
+  info.classList.remove('right-0');
+};
